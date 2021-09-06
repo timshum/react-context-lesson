97b2fb812e5ecc7dc96b2b9555a1b74dd53267a0
timshum <timshum@artytect.com>
2021-09-06 23:01:04 +0800

    Context API: useContext

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
@@ -7,24 +7,19 @@ import CollectionsContext from '../../contexts/collections/collections.context';
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
-  return (
-    <CollectionsContext.Consumer>
-      {(collections) => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
 
-        return (
-          <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-              {items.map((item) => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
